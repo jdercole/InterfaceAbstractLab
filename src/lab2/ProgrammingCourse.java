9a76e7c7b1a2aa1e792cc9b891e330e7fc5c0939
Genna1 <Genna1@Jenna.attlocal.net>
2017-09-17 22:09:27 -0500

    Lab 2: created ProgrammingCourse interface with body-less methods inherited by all sub-classes. Unfinished.

---
+++ src/lab2/ProgrammingCourse.java
@@ -0,0 +1,24 @@
+
+package lab2;
+
+/**
+ *
+ * @author Jenna
+ */
+public interface ProgrammingCourse {
+    public double getCredits();
+
+    public void setCredits(double credits);
+    
+    public int getCourseId();
+
+     public void setCourseId(int courseId);
+
+    public String getCourseName();
+
+    public void setCourseName(String courseName);
+
+    public String getInstructor();
+
+    public void setInstructor(String instructor);
+}
